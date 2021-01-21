3fcbda551c4daa08081f2a2895c8250e209cd8c5
tinkerbell93 <hhjj105@gmail.com>
2021-01-21 19:15:54 +0900

    TIL: Vue CLI form

---
+++ vue-form/src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <form action>
+    <div>
+      <label for="username">id:</label>
+      <input id="username" type="text" />
+    </div>
+    <div>
+      <label for="password">pw:</label>
+      <input id="password" type="password" />
+    </div>
+    <button>login</button>
+  </form>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+</style>
