bf0c1cad88da03e2e0662a138c6d63856865832f
tinkerbell93 <hhjj105@gmail.com>
2021-01-21 19:31:26 +0900

    TIL: Vue CLI form

--- vue-form/src/App.vue
+++ vue-form/src/App.vue
@@ -1,19 +1,47 @@
 <template>
-  <form action>
+  <form v-on:submit.prevent="submitForm">
     <div>
       <label for="username">id:</label>
-      <input id="username" type="text" />
+      <input id="username" type="text" v-model="username" />
     </div>
     <div>
       <label for="password">pw:</label>
-      <input id="password" type="password" />
+      <input id="password" type="password" v-model="password" />
     </div>
-    <button>login</button>
+    <button type="submit" v-on:click="submitForm">login</button>
   </form>
 </template>
 
 <script>
-export default {};
+import axios from "axios";
+
+export default {
+  data: function () {
+    return {
+      username: "",
+      password: "",
+    };
+  },
+  methods: {
+    submitForm: function () {
+      // e.preventDefault();
+      console.log(this.username, this.password);
+      const url = "https://jsonplaceholder.typicode.com/users";
+      const data = {
+        username: this.username,
+        password: this.password,
+      };
+      axios
+        .post(url, data)
+        .then(function (response) {
+          console.log(response);
+        })
+        .catch(function (error) {
+          console.log(error);
+        });
+    },
+  },
+};
 </script>
 
 <style>
